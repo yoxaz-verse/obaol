70e4e5df5195b0b77505935aca699d95f1d9e8e8
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-08-04 17:47:24 +0530

    Subs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,53 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+import mdx from "@next/mdx";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+
+  async rewrites() {
+    return [
+      {
+        source: "/overview",
+        destination: "https://overview.obaol.com",
+      },
+      {
+        source: "/overview/:path*",
+        destination: "https://overview.obaol.com/:path*",
+      },
+      {
+        source: "/onboarding",
+        destination: "https://onboarding.obaol.com",
+      },
+      {
+        source: "/onboarding/:path*",
+        destination: "https://onboarding.obaol.com/:path*",
+      },
+      {
+        source: "/automate",
+        destination: "https://automate.obaol.com",
+      },
+      {
+        source: "/automate/:path*",
+        destination: "https://automate.obaol.com/:path*",
+      },
+      {
+        source: "/cardamom",
+        destination: "https://cardamom.obaol.com",
+      },
+      {
+        source: "/gain",
+        destination: "https://gain.obaol.com",
+      },
+    ];
+  },
 };
 
 export default withNextIntl(withMDX(nextConfig));
