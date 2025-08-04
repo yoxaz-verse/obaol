66049f419776741eddf35a433b319d4834a99830
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-08-04 19:54:51 +0530

    Fix middleware and rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -18,59 +18,73 @@ nextConfig = Object.assign({}, nextConfig, {
       {
         source: "/overview",
         destination: "https://overview.obaol.com",
+        permanent: false,
       },
       {
         source: "/overview/:path*",
         destination: "https://overview.obaol.com/:path*",
+        permanent: false,
       },
       {
         source: "/onboarding",
         destination: "https://onboarding.obaol.com",
+        permanent: false,
       },
       {
         source: "/onboarding/:path*",
         destination: "https://onboarding.obaol.com/:path*",
+        permanent: false,
       },
       {
         source: "/automate",
         destination: "https://automate.obaol.com",
+        permanent: false,
       },
       {
         source: "/automate/:path*",
         destination: "https://automate.obaol.com/:path*",
+        permanent: false,
       },
       {
         source: "/cardamom",
         destination: "https://cardamom.obaol.com",
+        permanent: false,
       },
       {
         source: "/cardamom/:path*",
         destination: "https://cardamom.obaol.com/:path*",
+        permanent: false,
       },
       {
         source: "/gain",
         destination: "https://gain.obaol.com",
+        permanent: false,
       },
       {
         source: "/gain/:path*",
         destination: "https://gain.obaol.com/:path*",
+        permanent: false,
       },
       // localized paths, if desired
       {
         source: "/:locale/overview",
         destination: "https://overview.obaol.com",
+        permanent: false,
       },
       {
         source: "/:locale/overview/:path*",
         destination: "https://overview.obaol.com/:path*",
+        permanent: false,
       },
       {
         source: "/:locale/automate",
         destination: "https://automate.obaol.com",
+        permanent: false,
       },
       {
         source: "/:locale/automate/:path*",
         destination: "https://automate.obaol.com/:path*",
+        permanent: false,
       },
       // add more localized rewrites if needed ...
     ];
